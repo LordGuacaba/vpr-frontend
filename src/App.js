5ed39ad6eab66c9a6517af6d9f0c701f9e9a5809
Will Hoover <70659810+LordGuacaba@users.noreply.github.com>
2024-07-25 13:19:37 -0400

    ppts created on backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from "axios";
 
 function App() {
+
+  const [outputName, setOutputName] = useState("")
+  const [file, setFile] = useState()
+
+  const handleAddFile = e => {
+    setFile(e.target.files[0])
+  }
+
+  const handleNameChange = e => {
+    setOutputName(e.target.value)
+  }
+
+  const handleFileSubmission = e => {
+    const baseUrl = "http://localhost:8000";
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('fileName', file.name);
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    };
+    axios.post(`${baseUrl}/create?name=${outputName}`, formData, config)
+    .then(response => console.log(response.data))
+    .catch(error => console.error("Error: ", error))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Visual Packet Reader</h1>
+      <form onSubmit={handleFileSubmission}>
+        <h2>File submission</h2>
+        <input type="file" onChange={handleAddFile} />
+        <input type="text" onChange={handleNameChange} />
+        <button type="submit">Create Powerpoint</button>
+      </form>
     </div>
   );
 }
